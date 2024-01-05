8f34545830ca3be4bd7936c349d959bda978d626
ishopjapan <minhtuandang127@gmail.com>
2024-01-05 21:34:19 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="nav_wrapper">
+                <a href="#">
+                    <div class="logo">
+                        <img src="">
+                        <h5>foodie</h5>
+                    </div>
+                </a>
+                <div class="navigation">
+                    <div class="menu">
+                        <a href="#">Home</a>
+                        <a href="#">Foods</a>
+                        <a href="#">Cart</a>
+                        <a href="#">Contact</a>
+                    </div>
+                </div>
+                <div class="nav_right">
+                    <span class="icon">
+                        <i class="ri-shopping-basket-line"></i>
+                            <span class="badge">0</span>
+                    </span>
+                    <span class="user">
+                        <a href="#">
+                            <i class="ri-user-line"></i>
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
